fa8c329722a0ec85f0260552bdcae4fecd609ac3
Нурлан <nurlankurbanov53@gmail.com>
2022-10-31 03:51:18 +0300

    links are together

--- md_converter.py
+++ md_converter.py
@@ -1,10 +1,17 @@
 import pathlib
 
 INPUT_CODE_DELIMITER = "# ---end----"
+MD_CONTENT_DELIMITER = '<!---md_file_delimiter--->'
+
+
+def is_empty(data):
+    if len(data) == 0 or data == '\n':
+        return True
+    return False
 
 
 def read_data(file_name):
-    p = pathlib.Path("..") / file_name
+    p = pathlib.Path(".") / file_name
     file = p.open()
     content = file.read()
     file.close()
@@ -30,39 +37,55 @@ def prepare_md_titles(data):
 
 
 def prepare_md_format(title, description, code):
-    md_link = '-'.join(title.lower().split())
+    res = f"""## {title}
 
-    res = f"""+ [{title}](#{md_link})
-
-## {title}
 {description}
 
 ```python
 {code.strip()}
 ```
 """
-
     return res
 
 
-def convert_data(data):
+def prepare_md_link(title):
+    md_link = '-'.join(title.lower().split())
+    res = f"+ [{title}](#{md_link})"
+    return res
+
+
+def prepare_new_md_content(new_md_link, new_md_code, old_md_content):
+    if old_md_content is None:
+        result_md = f"{new_md_link}\n{MD_CONTENT_DELIMITER}\n\n{new_md_code}"
+    else:
+        old_md_link, old_md_code = old_md_content.split(MD_CONTENT_DELIMITER)
+        result_md = f"{old_md_link}{new_md_link}\n{MD_CONTENT_DELIMITER}{old_md_code}\n\n{new_md_code}"
+    return result_md
+
+
+def convert_data(data, old_md_content):
     file_info, code = data.split(INPUT_CODE_DELIMITER)
     title, description = prepare_md_titles(file_info)
-    res = prepare_md_format(title, description, code)
+    new_md_code = prepare_md_format(title, description, code)
+    new_md_link = prepare_md_link(title)
+    res = prepare_new_md_content(new_md_link, new_md_code, old_md_content)  # <-old_md_content
     return res
 
 
 def main():
-    # if not pathlib.Path.exists(pathlib.Path(".") / "collection.txt"):
-    #     exit(12)
-    task_file_name = "1_2.py"
-    # old_file_name = 'collection.txt'
+    task_file_name = "2_2.py"
+    old_file_name = "collection.md"
 
     content = read_data(task_file_name)
-    # old_content = read_data(old_file_name)
+    if not pathlib.Path.exists(pathlib.Path(".") / "collection.md"):
+        old_md_content = None
+    else:
+        old_md_content = read_data(old_file_name)
+        if is_empty(old_md_content):
+            old_md_content = None
 
-    res = convert_data(content)
-    write_data(task_file_name, res)
+    res = convert_data(content, old_md_content)
+    write_data(old_file_name, res)
 
 
 if __name__ == "__main__":
